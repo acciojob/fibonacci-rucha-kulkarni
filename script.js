6d4c7361b52d74bb5acd697ba6a3e50b86664031
rucha-kulkarni <118054484+rucha-kulkarni@users.noreply.github.com>
2023-07-11 23:43:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+
+let a = 0, b = 1;
 function fibonacci(num) {
-// your code here
+	if(num<=2){
+		return n-1;
+	}
+	for(let i = 0; i<num-2; i++){
+		let temp = b;
+		b = a + b;
+		a = temp;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
